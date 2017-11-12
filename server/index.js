2dddcd6899a405da557affd22e7ad483606005e0
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-11 23:43:08 -0800

    added more html/css

--- server/index.js
+++ server/index.js
@@ -4,16 +4,17 @@ const bodyParser = require('body-parser')
 const Promise = require('bluebird')
 const db = require('../database/index.js')
 const api = require('./unsplash.js')
+const cors = require('cors')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+  next()
 })
 
 app.get('/', (req, res) => res.send('please use proper endpoint'))
